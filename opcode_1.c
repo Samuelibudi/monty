e61807f53481e43d5c740d4fdddf580662e96359
Samuelibudi <ibudisamuel@gmail.com>
2022-12-15 03:39:12 -0800

    Adding files

--- opcode_1.c
+++ opcode_1.c
@@ -61,3 +61,38 @@ void _sub(stack_t **doubly, unsigned int cline)
 	aux->n -= (*doubly)->n;
 	_pop(doubly, cline);
 }
+
+/**
+ * _div - divides the second element by the top of the stack
+ * @doubly: head of the linked list
+ * @cline: line numbers
+ * Return: nothing
+ */
+void _div(stack_t **doubly, unsigned int cline)
+{
+	int m = 0;
+	stack_t *aux = NULL;
+
+	aux = *doubly;
+
+	for (; aux != NULL; aux = aux->next, m++)
+		;
+
+	if (m < 2)
+	{
+		fprintf(stderr, "L%u: can't div, stack too short\n", cline);
+		free_vg();
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*doubly)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", cline);
+		free_vg();
+		exit(EXIT_FAILURE);
+	}
+
+	aux = (*doubly)->next;
+	aux->n /= (*doubly)->n;
+	_pop(doubly, cline);
+}
