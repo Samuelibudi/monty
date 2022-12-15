6daefa4d0f7afe34b4427c931b8c18be576348a6
Samuelibudi <ibudisamuel@gmail.com>
2022-12-15 03:46:01 -0800

    adding files

--- monty.h
+++ monty.h
@@ -71,6 +71,7 @@ void _add(stack_t **stack, unsigned int line_number);
 void _nop(stack_t **stack, unsigned int line_number);
 void _sub(stack_t **stack, unsigned int line_number);
 void _div(stack_t **stack, unsigned int line_number);
+void _mul(stack_t **stack, unsigned int line_number);
 /*get functions*/
 void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
 
