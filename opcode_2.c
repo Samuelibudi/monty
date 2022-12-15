fc79f3a9c1523e4f5324ef3ae7c2136e7ee11df3
Samuelibudi <ibudisamuel@gmail.com>
2022-12-15 06:00:43 -0800

    Adding files

--- opcode_2.c
+++ opcode_2.c
@@ -85,3 +85,25 @@ void _pchar(stack_t **doubly, unsigned int cline)
 	}
 	printf("%c\n", (*doubly)->n);
 }
+
+/**
+ * _pstr - prints the string of the stack
+ * @doubly: head of the linked list
+ * @cline: line number
+ * Return: nothing
+ */
+void _pstr(stack_t **doubly, unsigned int cline)
+{
+	stack_t *aux;
+	(void)cline;
+
+	aux = *doubly;
+
+	while (aux && aux->n > 0 && aux->n < 128)
+	{
+		printf("%c", aux->n);
+		aux = aux->next;
+	}
+
+	printf("\n");
+}
