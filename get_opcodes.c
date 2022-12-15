3e3b3a5cb45f6940b5886f392d6de5b897f87b59
Samuelibudi <ibudisamuel@gmail.com>
2022-12-15 06:14:22 -0800

    Adding files

--- get_opcodes.c
+++ get_opcodes.c
@@ -21,6 +21,8 @@ void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number)
 		{"mod", _mod},
 		{"pchar", _pchar},
 		{"pstr", _pstr},
+		{"rotl", _rotl},
+		{"rotr", _rotr},
 		{NULL, NULL}
 	};
 	int i;
